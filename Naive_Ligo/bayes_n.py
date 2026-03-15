edde93b3e68b9359747fc7ad9a0e3ba302756391
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-03-15 20:53:52 +0800

    Feat: totalYN_col (returns the total yes and no under circumstances)

--- Naive_Ligo/bayes_n.py
+++ Naive_Ligo/bayes_n.py
@@ -3,18 +3,31 @@ import pandas as pd
 
 
 def total_YN(outlook, decision):
+    """Returns the total yes or no in the Maligo col"""
+
     count = (outlook == decision).sum()
 
     return count
+
+def totalYN_col(col, val, decision):
+    """Returns how many yes or no in a circumstances(cols)"""
+
+    condition = (col == val) & (col == decision)
+
+    return condition.sum()
 
 
 df = pd.read_csv("Naive_Ligo/maligo_data.csv")
 
 
 # Total Yes or No
-total_yes = total_YN(df['Maligo'], "Yes")
-total_no = total_YN(df['Maligo'], "No")
+total_yes = total_YN(df['Maligo'], "Yes")       # 7
+total_no = total_YN(df['Maligo'], "No")         # 4
 
-
+# Total Yes and No for the Rainy
+total_yes_rainy = totalYN_col(df['Weather'], "" "Yes")
+total_no_rainy = totalYN_col(df, "No")
 
 print(df)
+print("\n\n\n", total_yes_rainy)
+print(total_no_rainy)
