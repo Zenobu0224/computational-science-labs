bfc51fd8220c5b9cd1fa457b1e54da50b5379255
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-03-15 21:01:01 +0800

    Get the variables for weather

--- Naive_Ligo/bayes_n.py
+++ Naive_Ligo/bayes_n.py
@@ -9,10 +9,10 @@ def total_YN(outlook, decision):
 
     return count
 
-def totalYN_col(col, val, decision):
+def totalYN_col(df, col, val, decision):
     """Returns how many yes or no in a circumstances(cols)"""
 
-    condition = (col == val) & (col == decision)
+    condition = (col == val) & (df['Maligo'] == decision)
 
     return condition.sum()
 
@@ -25,9 +25,18 @@ total_yes = total_YN(df['Maligo'], "Yes")       # 7
 total_no = total_YN(df['Maligo'], "No")         # 4
 
 # Total Yes and No for the Rainy
-total_yes_rainy = totalYN_col(df['Weather'], "" "Yes")
-total_no_rainy = totalYN_col(df, "No")
+total_yes_rainy = totalYN_col(df, df['Weather'], "Rainy", "Yes")        # 2
+total_no_rainy = totalYN_col(df, df['Weather'], "Rainy", "No")          # 2
+
+# Total Yes and No for the Sunny    
+total_yes_sunny = totalYN_col(df, df['Weather'], "Sunny", "Yes")        # 3
+total_no_sunny = totalYN_col(df, df['Weather'], "Sunny", "No")          # 1
+
+# Total yes and No for the Cloudy
+total_yes_cloudy = totalYN_col(df, df['Weather'], "Cloudy", "Yes")      # 2
+total_no_cloudy = totalYN_col(df, df['Weather'], "Cloudy", "No")        # 1
+
 
 print(df)
-print("\n\n\n", total_yes_rainy)
-print(total_no_rainy)
+print("\n\n\n", total_yes_cloudy)
+print(total_no_cloudy)
